4d9c13b81cde0200dc1afd026bfccdf5e8ad2db8
pepo <pepo@ukr.net>
2019-05-30 23:07:57 +0300

    const

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 
 let main = document.querySelector('.img_big');
 let img = document.querySelectorAll('.img_small');
-let res = main.src;
+const res = main.src;
 for(let i = 0; i < img.length; i++ ) {
     img[i].addEventListener( 'mouseover', function(){
         main.src = img[i].src;
     })
+    
     img[i].addEventListener( 'mouseout', function(){
         main.src = res;
     })
